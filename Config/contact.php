d238a2d3d1891ff0142870b31c14894f4d9ca025
Johannes Braun <me@hannenz.de>
2012-01-14 10:20:45 +0100

    Added comments

--- Config/contact.php
+++ Config/contact.php
@@ -1,17 +1,36 @@
 <?php
+/**
+ * ContactPlugin Configuration
+ */
+
 $config = array(
 	'ContactPlugin' => array(
+		// Whether or not messages should be saved to Database
 		'saveDb' => true,
+
+		// Redirect after message has been sent
 		'redirect' => '/',
+
+		// Sender of the message(s)
 		'sender' => 'noreply@'.$_SERVER['SERVER_NAME'],
+
+		// Recipient(s) of the message(s)
 		'recipients' => array(
 			'me@hannenz.de'
+			//~ 'info@'.$_SERVER['SERVER_NAME']
 		),
+
+		// The message's subject
 		'subject' => __('Contact request'),
+
+		// Flash message and misc. customizable strings
 		'flashSuccess' => __('Your message has been sent'),
 		'flashFailure' => __('Failed to send message'),
 		'submitText' => __('Send message'),
 		'validationErrorFstr' => __('Please check the field "%s": %s'),
+
+		// The fields of the contact form, as passed to
+		// FormHelper::input
 		'fields' => array(
 			'name',
 			'email',
@@ -19,6 +38,8 @@ $config = array(
 				'type' => 'textarea'
 			)
 		),
+
+		// Validation rules for the fields specified above as passed to Model
 		'validate' => array(
 			'name' => array(
 				'rule' => 'notEmpty',
